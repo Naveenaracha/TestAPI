0a6caad510c0a4aaaf3f3c7f0aa4847b64fae118
Naveenaracha <167506750+Naveenaracha@users.noreply.github.com>
2024-12-15 15:53:19 +0530

    Fin

--- test_scheduler.py
+++ test_scheduler.py
@@ -34,4 +34,5 @@ def schedule_class():
     return jsonify(response_data), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port, debug=True)
